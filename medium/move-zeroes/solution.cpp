e0a64b3508fa49d4efaf7e10614564606b53c5ae
rehyan_yadav <67955608+Rehyanyadav@users.noreply.github.com>
2026-08-05 20:16:58 +0530

    ✅ Move Zeroes | Medium | C++ | Runtime: 0 ms | Memory: 8.4 MB | 2026-08-05

--- medium/move-zeroes/solution.cpp
+++ medium/move-zeroes/solution.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
- for (int i =0 ; i<nums.size(); i++){
-    if(nums[i] == 0){
-        for(int j = i+1; j<nums.size(); j++){
-            if(nums[j] != 0){
-                swap(nums[i],nums[j]);
-
+         //* efficient solution 
+         int count = 0;
+         for(int i =0; i<nums.size(); i++){
+            if(nums[i] != 0){
+         }
+                swap(nums[i] , nums[count]);
             }
-        }
     }
- }
-    }
+                count ++;
+                
 };
