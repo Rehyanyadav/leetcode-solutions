3b3af829ab907929a2459b65e986b933f704bef7
rehyan_yadav <67955608+Rehyanyadav@users.noreply.github.com>
2026-08-06 21:47:23 +0530

    ✅ Rotate Array | Medium | C++ | Runtime: 0 ms | Memory: 8.2 MB | 2026-08-06

--- medium/0189-rotate-array/solution.cpp
+++ medium/0189-rotate-array/solution.cpp
@@ -1,14 +1,14 @@
-class Solution {
+class Solution {
 public:
-    void rotate(vector<int>& nums, int k) {
-int temp[k];
-    temp[i] = nums[i];
- 
-     }
-  for(int i =0; i<k; i++)
-    for(int i =k; i<nums.size(); i++)
-       nums[i-k] = nums[i];
-    for(int i = 0; i<k; i++)
-        nums[nums.size() - k +i] = temp[i];
+    void rotate(vector<int>& nums, int k) {
+int temp[k];
+  for(int i =0; i<k; i++)
+    temp[i] = nums[i];
+    for(int i =k; i<nums.size(); i++)
+       nums[i-k] = nums[i];
+    for(int i = 0; i<k; i++)
+        nums[nums.size() - k +i] = temp[i];
 
+ 
+     }
 };
