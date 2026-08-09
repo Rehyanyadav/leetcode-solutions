94f04ac3dc5c8a7e88382f136f50280223f57b82
rehyan_yadav <67955608+Rehyanyadav@users.noreply.github.com>
2026-08-09 14:56:59 +0530

    ✅ Sort Array by Increasing Frequency | Easy | C++ | Runtime: 0 ms | Memory: 8.3 MB | 2026-08-09

---
+++ easy/1636-sort-array-by-increasing-frequency/solution.cpp
@@ -0,0 +1,15 @@
+
+}
+sort(nums.begin(),nums.end(), [&count]
+(int a , int b){
+    if(count[a ] == count[b]){
+        return a>b;
+
+    }
+     return count[a] < count [b];
+
+    count[num]++;
+for(int num :nums){
+unordered_map<int , int > count;
+    (vector<int>& nums) {
+    vector<int> frequencySort
