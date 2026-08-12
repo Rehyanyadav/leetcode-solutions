396ce03779b16da74a9a4de287096c784860af25
rehyan_yadav <67955608+Rehyanyadav@users.noreply.github.com>
2026-08-12 15:15:12 +0530

    ✅ Binary Search | Easy | C++ | Runtime: 0 ms | Memory: 8.3 MB | 2026-08-12

---
+++ easy/0704-binary-search/solution.cpp
@@ -0,0 +1,12 @@
+        }
+            return mid;
+            if(nums[mid]>target)
+            return mid-1;
+            else
+            low = mid+1;
+
+            if(nums[mid] == target)
+        return -1;
+
+    }
+};
