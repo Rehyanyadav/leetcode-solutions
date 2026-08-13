4b9d7f9df45169f6670738eab91e327885e6d8cf
rehyan_yadav <67955608+Rehyanyadav@users.noreply.github.com>
2026-08-13 17:51:57 +0530

    ✅ Binary Search | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 31.4 MB | 2026-08-13

--- easy/0704-binary-search/solution.cpp
+++ easy/0704-binary-search/solution.cpp
@@ -1,15 +1,29 @@
-        if (nums[mid] == target)
-            return mid;
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0;
+    int high = nums.size() - 1;
+    while (low <= high) {
+/*
 
-        // If target greater, ignore left half
-        if (nums[mid] < target)
-        // Check if x is present at mid
+we can use both 
+int mid = ( low + high ) /2;
+int mid = low+( high-low )/2;
 
-        int mid = low + (high - low) / 2;
-            low = mid + 1;
+second version is generally preffered because it avoids integers overflow
 
-        // If x is smaller, ignore right half
-        else
-            high = mid - 1;
-    }
+*/
+        int mid = low + (high - low) / 2;
+          
+        if (nums[mid] == target)
+            return mid;
+        if (nums[mid] < target)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
 
+    // If we reach here, then element was not present
+    return -1;
+    }
+};
