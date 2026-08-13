c348e4dcaa9cca3dad687fd0f9e63a555c9a28f2
rehyan_yadav <67955608+Rehyanyadav@users.noreply.github.com>
2026-08-13 17:43:25 +0530

    ✅ Binary Search | Easy | C++ | Runtime: 0 ms | Memory: 8.4 MB | 2026-08-13

--- easy/0704-binary-search/solution.cpp
+++ easy/0704-binary-search/solution.cpp
@@ -1,12 +1,15 @@
-                return mid+1;
-            else
-                return high= mid-1;
-        }
-            if(nums[mid]>target)
-                return mid;
-            if(nums[mid] == target)
-         int mid = low+(high-low)/2;
-         return -1;
+        if (nums[mid] == target)
+            return mid;
+
+        // If target greater, ignore left half
+        if (nums[mid] < target)
+        // Check if x is present at mid
+
+        int mid = low + (high - low) / 2;
+            low = mid + 1;
+
+        // If x is smaller, ignore right half
+        else
+            high = mid - 1;
     }
-         
-};
+
