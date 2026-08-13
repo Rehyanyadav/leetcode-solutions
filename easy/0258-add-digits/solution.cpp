4a3b96aac7edf16071a23257d56c4a24d1df181c
rehyan_yadav <67955608+Rehyanyadav@users.noreply.github.com>
2026-08-13 21:56:00 +0530

    ✅ Add Digits | Easy | C++ | Runtime: 0 ms | Memory: 7.7 MB | 2026-08-13

--- easy/0258-add-digits/solution.cpp
+++ easy/0258-add-digits/solution.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     int addDigits(int num) {
-        int result = (num+1)/2;
-    }
-        return result;
 
+        int result = (num+1)/2;
+        result = num*result;
+        
+        return result;
+    }
 };
