f9ad61f9eb3618dd975629fd81fdc9636539eefd
rehyan_yadav <67955608+Rehyanyadav@users.noreply.github.com>
2026-08-13 21:49:13 +0530

    ✅ Add Binary | Easy | C++ | Runtime: 3 ms (18.24%) | Memory: 9.9 MB | 2026-08-13

--- easy/0067-add-binary/solution.cpp
+++ easy/0067-add-binary/solution.cpp
@@ -1,11 +1,44 @@
+class Solution {
+public:
 
-            i--;
-            j--;
-        }
+ int charToInt(char c) {
+        return c - '0';
+    }
 
 
-reverse(result.begin(), result.end());
+    int getBit(const std::string& str, int index) {
+        if (index >= 0) {
+            return charToInt(str[index]);
+        }
+        return 0; // Return 0 if we run out of digits on the left
+    }
+    string addBinary(string a, string b) {
+           string result = "";
+        
 
-        return result;
-    }
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+
+        while (i >= 0 || j >= 0 || carry > 0) {
+
+            int sum = carry + getBit(a, i) + getBit(b, j);
+
+
+            result += std::to_string(sum % 2);
+
+
+            carry = sum / 2;
+
+
+            i--;
+            j--;
+        }
+
+
+reverse(result.begin(), result.end());
+
+        return result;
+    }
 };
