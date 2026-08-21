6d41f0b888c2034c3a66988c003bb86b6091b395
rehyan_yadav <67955608+Rehyanyadav@users.noreply.github.com>
2026-08-21 15:47:28 +0530

    ✅ Sqrt(x) | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 8.6 MB | 2026-08-21

--- easy/0069-sqrtx/solution.cpp
+++ easy/0069-sqrtx/solution.cpp
@@ -1,12 +1,28 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        int i =1;
-        while(i*i<=x){
-            i++;
+        int low = 0;
+        int high = x;
+        int ans = -1;
+        while(low <= high){
+            long long mid = low+(high-low)/2;
+            long long msq = mid * mid;
+            if(msq == x){
+                return mid;
+            }
+            else if(msq <x){
+                ans = mid;
+                low = mid+1;
 
+            }
+            else{
+                high = mid-1;
+            }
 
         }
-             return (i-1);
+
+        
+        return ans;
+
     }
 };
