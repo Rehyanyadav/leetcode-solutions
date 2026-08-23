871dc48d4634d4a417ee047e5755aa59ae1f82b1
rehyan_yadav <67955608+Rehyanyadav@users.noreply.github.com>
2026-08-23 23:47:15 +0530

    ✅ Assign Cookies | Easy | C++ | Runtime: 10 ms (44.67%) | Memory: 44.9 MB | 2026-08-23

--- easy/0455-assign-cookies/solution.cpp
+++ easy/0455-assign-cookies/solution.cpp
@@ -1,27 +1,43 @@
-//             }
-// left++;
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+//          int n =  g.size(); 
+//          int m =  s.size();
 
-//          }
+// int left = 0 ,right = 0;
+//          sort(g.begin(),g.end());
+//          sort(s.begin(),s.end());
+         
+//          while(left< m  &&  right <n){
 
-//          return right;
-         
+//             if(g[right]<= s[left]){
+//                 right++;
+
+//             }
+// left++;
+
+//          }
+
+//          return right;
+         
 
 
 
 
-         sort(g.begin(), g.end());
-         sort(s.begin(), s.end());
-         int left = 0; int right = 0;
-         while(left<s.size()  && right <g.size()){
-            if(g[right]<= s[left]){
-                right++;
-            }
+         sort(g.begin(), g.end());
+         sort(s.begin(), s.end());
+         int left = 0; int right = 0;
+         while(left<s.size()  && right <g.size()){
+            if(g[right]<= s[left]){
+                right++;
 
+            }
 
-            left++;
+            left++;
 
-         }
+         }
 
-         return right;
+         return right;
 
-    }
+    }
+};
