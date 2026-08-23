a14daaf66794771a179975195ffb66e0acbf1eae
rehyan_yadav <67955608+Rehyanyadav@users.noreply.github.com>
2026-08-23 22:50:47 +0530

    ✅ Assign Cookies | Easy | C++ | Runtime: 0 ms | Memory: 8.3 MB | 2026-08-23

---
+++ easy/0455-assign-cookies/solution.cpp
@@ -0,0 +1,25 @@
+
+            if(g[right]<= s[left]){
+                right++;
+         while(left< m  &&  right <n){
+         
+         sort(s.begin(),s.end());
+
+            }
+left++;
+
+         sort(g.begin(),g.end());
+int right = 0;
+int left = 0;
+
+         int m =  s.size();
+         }
+
+    }
+         int n =  g.size(); 
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+public:
+class Solution {
+         return right;
+         
+};
