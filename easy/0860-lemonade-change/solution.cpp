3e2e346ec85c7012a9c257671e6128a3fa853d77
rehyan_yadav <67955608+Rehyanyadav@users.noreply.github.com>
2026-08-24 20:44:20 +0530

    ✅ Lemonade Change | Easy | C++ | Runtime: 0 ms | Memory: 7.9 MB | 2026-08-24

---
+++ easy/0860-lemonade-change/solution.cpp
@@ -0,0 +1,25 @@
+            five--;
+        }else{
+            ten++;
+
+            return false;
+        }
+            five--;
+        }else if(five >=3){
+            ten--;
+
+            five -= 3;
+        }else{
+
+            return false;
+        }
+
+        }else if(five>0 && ten >0){
+
+
+    
+}
+    }
+return true;
+
+};
