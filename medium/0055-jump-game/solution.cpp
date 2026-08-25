ce22d3e8a823b500f6b49f753750e4d9dd2bf3eb
rehyan_yadav <67955608+Rehyanyadav@users.noreply.github.com>
2026-08-25 08:37:50 +0530

    ✅ Jump Game | Medium | C++ | Runtime: 0 ms (100.00%) | Memory: 52.5 MB | 2026-08-25

--- medium/0055-jump-game/solution.cpp
+++ medium/0055-jump-game/solution.cpp
@@ -1,26 +1,26 @@
-class Solution {
+class Solution {
 public:
-    bool canJump(vector<int>& nums) {
-//  here  we have to  keep the maximum index we have reach
-int max_Reach =0;
+    bool canJump(vector<int>& nums) {
+//  here  we have to  keep the maximum index we have reach
+int max_Reach =0;
 
 
-// loop till end to the size of array 
+// loop till end to the size of array 
 
-for(int i =0; i<nums.size(); i++){
-    //here we check  our current i is excced more than max_Reach
-    if(i >max_Reach){
-        // then we will return false
-        return false;
+for(int i =0; i<nums.size(); i++){
+    //here we check  our current i is excced more than max_Reach
+    if(i >max_Reach){
+        // then we will return false
+        return false;
 
-    }
+    }
 
-// now here we will  check  how much farthest jump we are geeting  and set it 
-max_Reach = max(max_Reach , i+ nums[i]);
+// now here we will  check  how much farthest jump we are geeting  and set it 
+max_Reach = max(max_Reach , i+ nums[i]);
 
 }
 
 
-return true;
-    }
+return true;
+    }
 };
