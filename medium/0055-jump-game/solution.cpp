906d44a17d82065d7e967c64373fa99bc26e740f
rehyan_yadav <67955608+Rehyanyadav@users.noreply.github.com>
2026-08-25 05:32:42 +0530

    ✅ Jump Game | Medium | C++ | Runtime: 0 ms (100.00%) | Memory: 52.4 MB | 2026-08-25

--- medium/0055-jump-game/solution.cpp
+++ medium/0055-jump-game/solution.cpp
@@ -1,16 +1,16 @@
-class Solution {
+class Solution {
 public:
-    bool canJump(vector<int>& nums) {
-int max_Reach =0;
-for(int i =0; i<nums.size(); i++){
-    if(i >max_Reach){
-        return false;
-    }
+    bool canJump(vector<int>& nums) {
+int max_Reach =0;
+for(int i =0; i<nums.size(); i++){
+    if(i >max_Reach){
+        return false;
+
+    }
+
+max_Reach = max(max_Reach , i+ nums[i]);
+
 }
-
-
-    }
-max_Reach = max(max_Reach , i+ nums[i]);
-
-return true;
+return true;
+    }
 };
