c3bd2f31b92ddfecab8cfae11e83a38b77f09bd2
rehyan_yadav <67955608+Rehyanyadav@users.noreply.github.com>
2026-08-25 08:32:15 +0530

    ✅ Jump Game | Medium | C++ | Runtime: 0 ms (100.00%) | Memory: 52.2 MB | 2026-08-25

--- medium/0055-jump-game/solution.cpp
+++ medium/0055-jump-game/solution.cpp
@@ -8,14 +8,19 @@ int max_Reach =0;
 // loop till end to the size of array 
 
 for(int i =0; i<nums.size(); i++){
+    //here we check  our current i is excced more than max_Reach
     if(i >max_Reach){
+        // then we will return false
         return false;
 
     }
 
+// now here we will  check  how much farthest jump we are geeting  and set it 
 max_Reach = max(max_Reach , i+ nums[i]);
 
 }
+
+
 return true;
     }
 };
