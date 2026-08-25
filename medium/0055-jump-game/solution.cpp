556c9554c74e2f669697118e6a5075f0142ae737
rehyan_yadav <67955608+Rehyanyadav@users.noreply.github.com>
2026-08-25 08:27:18 +0530

    ✅ Jump Game | Medium | C++ | Runtime: 0 ms (100.00%) | Memory: 52.4 MB | 2026-08-25

--- medium/0055-jump-game/solution.cpp
+++ medium/0055-jump-game/solution.cpp
@@ -1,7 +1,12 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
+//  here  we have to  keep the maximum index we have reach
 int max_Reach =0;
+
+
+// loop till end to the size of array 
+
 for(int i =0; i<nums.size(); i++){
     if(i >max_Reach){
         return false;
