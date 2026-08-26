ac8578146c0979bb4ae26d819118a17025d481ac
rehyan_yadav <67955608+Rehyanyadav@users.noreply.github.com>
2026-08-26 22:19:12 +0530

    ✅ Jump Game II | Medium | C++ | Runtime: 0 ms | Memory: 8 MB | 2026-08-26

--- medium/0045-jump-game-ii/solution.cpp
+++ medium/0045-jump-game-ii/solution.cpp
@@ -5,12 +5,12 @@ public:
         for(int i =0; i<nums.size(); i++){
              if(i>max_reach){
                 return false;
-             }
-        }
-    }
 
+             }
              max_reach = max(max_reach , i -nums[i]);
+
+        }
         return true;
         
-
+    }
 };
