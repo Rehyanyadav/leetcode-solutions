67c422fcd856f603a587ea099a91cebd8f70212d
rehyan_yadav <67955608+Rehyanyadav@users.noreply.github.com>
2026-08-30 21:44:44 +0530

    ✅ Non-overlapping Intervals | Medium | C++ | Runtime: 48 ms (49.99%) | Memory: 94.1 MB | 2026-08-30

--- medium/0435-non-overlapping-intervals/solution.cpp
+++ medium/0435-non-overlapping-intervals/solution.cpp
@@ -1,18 +1,34 @@
-for(int i =1; i<intervals.size(); i++){
-    if(intervals[i][0] >= last_end_time){
-
-
-      int last_end_time = intervals[0][1];
-
-
-      int removed_count = 0;
-      sort (intervals.begin(), intervals.end(),compare);
+class Solution {
+public:
+static bool compare (vector<int> & a, vector<int> & b){
+    return a[1]< b[1];
 
 }
-    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-      if(intervals.empty()) return 0;
-    return a[1]< b[1];
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+      if(intervals.empty()) return 0;
+      sort (intervals.begin(), intervals.end(),compare);
 
-public:
-static bool compare (vector<int> & a, vector<int> & b){
-class Solution {
+
+      int removed_count = 0;
+      int last_end_time = intervals[0][1];
+
+
+
+for(int i =1; i<intervals.size(); i++){
+    if(intervals[i][0] >= last_end_time){
+ last_end_time = intervals[i][1];
+    }
+    else{
+        removed_count++;
+
+    }
+   
+
+}
+
+ return removed_count;
+
+
+
+    }
+};
