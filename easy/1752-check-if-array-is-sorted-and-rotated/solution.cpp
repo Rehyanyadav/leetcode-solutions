2be9657fb4f2f8cb12fb103a91646d992f27dd54
rehyan_yadav <67955608+Rehyanyadav@users.noreply.github.com>
2026-09-01 04:08:27 +0530

    ✅ Check if Array Is Sorted and Rotated | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 11.3 MB | 2026-08-31

--- easy/1752-check-if-array-is-sorted-and-rotated/solution.cpp
+++ easy/1752-check-if-array-is-sorted-and-rotated/solution.cpp
@@ -1,17 +1,16 @@
-class Solution {
+class Solution {
 public:
-    bool check(vector<int>& nums) {
-        int n = nums.size();
-        int drops = 0;
-        for(int i =0; i<nums.size(); i++){
-            // comparing element to the next element 
-        }
-            (cicular way)
-            if(nums[i]> nums[(i+1) % n] ){
-                drops++;
-            }
-    }
+    bool check(vector<int>& nums) {
+        int n = nums.size();
+        int drops = 0;
+        for(int i =0; i<nums.size(); i++){
+            // comparing element to the next element (cicular way)
+            if(nums[i]> nums[(i+1) % n] ){
+                drops++;
 
-        return drops<=1;
+            }
+        }
+        return drops<=1;
 
+    }
 };
