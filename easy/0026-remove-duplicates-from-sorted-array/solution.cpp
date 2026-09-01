292ae7eafcd0ac3896bb1f881850ea843d370f53
rehyan_yadav <67955608+Rehyanyadav@users.noreply.github.com>
2026-09-02 04:43:00 +0530

    ✅ Remove Duplicates from Sorted Array | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 22.6 MB | 2026-09-01

--- easy/0026-remove-duplicates-from-sorted-array/solution.cpp
+++ easy/0026-remove-duplicates-from-sorted-array/solution.cpp
@@ -1,23 +1,29 @@
-        
-        int i =0;  // creating the slow pointer which will be  keep track of the  unique 
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        
+        //* here we checked array is empty or not 
+        if(nums.empty()) return 0;
 
-        for( int j =0; j<nums.size(); j++){
-             if(nums[j]!= nums[i]){
-        }
-                i++;
-             }
-                nums[i] = nums[j];
+        int i =0;  // creating the slow pointer which will be  keep track of the  unique element
+
+     //here we traverse through the array  by using second pointer  
+        for( int j =0; j<nums.size(); j++){
+            // if  jth element is not equal to ith element 
+             if(nums[j]!= nums[i]){
+    // increment the i
+                i++;
+
+                // and overwrite  the duplicate element with new element
+                nums[i] = nums[j];
 
 
-        //* here we checked array is empty or not 
-        if(nums.empty()) return 0;
-        element
-     //here we traverse through the array  by using second pointer  
-            // if  jth element is not equal to ith element 
-    // increment the i
+             }
+        }
 
-                // and overwrite  the duplicate element with new element
+    // then return the unique element 
+         return  (i+1);
 
 
-    // then return the unique element 
-         return  (i+1);
+    }
+};
