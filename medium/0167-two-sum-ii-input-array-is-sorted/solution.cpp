0b501ac8c2aaa4ff7f842fda7839dc9d15c1b8ff
rehyan_yadav <67955608+Rehyanyadav@users.noreply.github.com>
2026-09-02 04:17:02 +0530

    ✅ Two Sum II - Input Array Is Sorted | Medium | C++ | Runtime: 0 ms (100.00%) | Memory: 19.5 MB | 2026-09-01

--- medium/0167-two-sum-ii-input-array-is-sorted/solution.cpp
+++ medium/0167-two-sum-ii-input-array-is-sorted/solution.cpp
@@ -1,18 +1,18 @@
-class Solution {
+class Solution {
 public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
+    vector<int> twoSum(vector<int>& numbers, int target) {
 
 
-            int l = 0;
-    int r = numbers.size() - 1;
+            int l = 0;
+    int r = numbers.size() - 1;
 
-    while (numbers[l] + numbers[r] != target)
-      if (numbers[l] + numbers[r] < target)
-        ++l;
-      else
-        --r;
+    while (numbers[l] + numbers[r] != target)
+      if (numbers[l] + numbers[r] < target)
+        ++l;
+      else
+        --r;
 
-    return {l + 1, r + 1};
+    return {l + 1, r + 1};
 
-    }
+    }
 };
