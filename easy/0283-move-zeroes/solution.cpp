405342675ebaabbca7abbc3df11d23ccb392caa9
rehyan_yadav <67955608+Rehyanyadav@users.noreply.github.com>
2026-09-04 17:22:45 +0530

    ✅ Move Zeroes | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 23.9 MB | 2026-09-04

--- easy/0283-move-zeroes/solution.cpp
+++ easy/0283-move-zeroes/solution.cpp
@@ -1,14 +1,14 @@
-class Solution {
+class Solution {
 public:
-    void moveZeroes(vector<int>& nums) {
-          //* efficient solution 
-         int count = 0;
-         for(int i =0; i<nums.size(); i++){
-            if(nums[i] != 0){
-                swap(nums[i] , nums[count]);
-                count ++;
-                
-            }
-         }
-    }
+    void moveZeroes(vector<int>& nums) {
+          //* efficient solution 
+         int count = 0;
+         for(int i =0; i<nums.size(); i++){
+            if(nums[i] != 0){
+                swap(nums[i] , nums[count]);
+                count ++;
+                
+            }
+         }
+    }
 };
