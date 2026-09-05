0c7734ff45550c91723f8c759b8f44935f481496
rehyan_yadav <67955608+Rehyanyadav@users.noreply.github.com>
2026-09-05 10:56:43 +0530

    ✅ Max Consecutive Ones | Easy | C++ | Runtime: 3 ms (17.99%) | Memory: 50.3 MB | 2026-09-05

--- easy/0485-max-consecutive-ones/solution.cpp
+++ easy/0485-max-consecutive-ones/solution.cpp
@@ -1,18 +1,18 @@
-class Solution {
+class Solution {
 public:
-    int findMaxConsecutiveOnes(vector<int>& nums) {
-        int count = 0;
-        int maxi = 0;
-        
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == 1) {
-                count++;
-                maxi = max(maxi, count); // Is my current streak a new record?
-            } else {
-                count = 0; // Streak broken, reset to 0
-            }
-        }
-        
-        return maxi;
-    }
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count = 0;
+        int maxi = 0;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 1) {
+                count++;
+                maxi = max(maxi, count); // Is my current streak a new record?
+            } else {
+                count = 0; // Streak broken, reset to 0
+            }
+        }
+        
+        return maxi;
+    }
 };
