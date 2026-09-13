df311e298af8b567dd6c7561c3eb3a7a91e5c8aa
rehyan_yadav <67955608+Rehyanyadav@users.noreply.github.com>
2026-09-13 10:19:59 +0530

    ✅ Next Permutation | Medium | C++ | Runtime: 0 ms | Memory: 8.2 MB | 2026-09-13

---
+++ medium/0031-next-permutation/solution.cpp
@@ -0,0 +1,21 @@
+         }
+           break;
+             }
+          if(ind ==1){
+           reverse(nums.begin() , nums.end());
+            }
+            return;
+       for(int i  =n-1; i>ind; i--){
+         if(nums[i]> nums[ind]){
+            swap(nums[i], nums[ind]);
+            break;
+             }
+           ind = i;
+          if(nums[i]<nums[i+1]){
+       for(int i =0; i<n; i++){
+          }
+     reverse(nums.begin() + ind+1 , nums.end());
+
+
+    }
+};
