68de2aee767ed0c21b66c1562a8b63bd3157bb14
rehyan_yadav <67955608+Rehyanyadav@users.noreply.github.com>
2026-09-15 00:36:18 +0530

    ✅ Next Permutation | Medium | C++ | Runtime: 0 ms (100.00%) | Memory: 15.7 MB | 2026-09-13

--- medium/0031-next-permutation/solution.cpp
+++ medium/0031-next-permutation/solution.cpp
@@ -1,22 +1,32 @@
-        }
-        
-        if (ind == -1) {
-        // If no break-point exists, reverse the 
-            reverse(nums.begin(), nums.end());
-            return;
-        }
-        
-        // Step 2: Find the next greater element and 
-        for (int i = n - 1; i > ind; i--) {
-            if (nums[i] > nums[ind]) {
-                swap(nums[i], nums[ind]);
-                break;
-        entire array
-        swap
-            }
-        }
-        
-        // Step 3: Reverse the right half
-        reverse(nums.begin() + ind + 1, nums.end());
-    }
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int ind = -1;
+        
+        // Step 1: Find the break-point
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                ind = i;
+                break;
+            }
+        }
+        
+        // If no break-point exists, reverse the entire array
+        if (ind == -1) {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+        
+        // Step 2: Find the next greater element and swap
+        for (int i = n - 1; i > ind; i--) {
+            if (nums[i] > nums[ind]) {
+                swap(nums[i], nums[ind]);
+                break;
+            }
+        }
+        
+        // Step 3: Reverse the right half
+        reverse(nums.begin() + ind + 1, nums.end());
+    }
 };
